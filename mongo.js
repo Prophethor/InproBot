d68f8135fb8c255ff2ae8b50a57ac00b44d490dc
Prophethor <prophethor@gmail.com>
2021-04-19 13:59:46 +0200

    Removed config.json and setup mongodb server

--- mongo.js
+++ mongo.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
-const { mongoPath} = require('./config.json')
 
 module.exports = async () => {
-    await mongoose.connect(mongoPath, {
+    await mongoose.connect(`mongodb+srv://Prophethor:${process.env.MONGO_PASS}@r2d2.kwnug.mongodb.net/r2d3`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
